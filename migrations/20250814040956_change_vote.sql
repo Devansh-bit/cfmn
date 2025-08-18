fe0b4151919f6ed2214a1023a922f96475125f13
Devansh-bit <devanshg308@gmail.com>
2025-08-18 15:42:00 +0530

    remove unneeded migrations

--- migrations/20250814040956_change_vote.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE votes;
-CREATE TABLE votes (
-                       id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-                       user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-                       note_id UUID NOT NULL REFERENCES notes(id) ON DELETE CASCADE,
-                       is_upvote BOOLEAN NOT NULL,
-                       created_at TIMESTAMPTZ DEFAULT NOW(),
-                       UNIQUE(user_id, note_id)
-);
--- migrations/20250816145253_delete_users.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE users
-DROP COLUMN picture;
-
-ALTER TABLE users
-ADD COLUMN picture TEXT NOT NULL DEFAULT 'https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg';
